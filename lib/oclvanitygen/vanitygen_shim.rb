581af9538b9a1b041a5c2a4a8e3f874ecfdc666b
fengb <contact@fengb.info>
2014-11-12 12:24:46 -0600

    Sanity check for shim

--- lib/oclvanitygen/vanitygen_shim.rb
+++ lib/oclvanitygen/vanitygen_shim.rb
@@ -1,4 +1,12 @@
 require 'oclvanitygen'
 
+if defined? ::Vanitygen
+  if ::Vanitygen == ::Oclvanitygen
+    warn 'warning: Vanitygen already defined. Possible double load issue.'
+  else
+    raise LoadError.new('Vanitygen already defined. Refusing to clobber.')
+  end
+end
+
 ::Vanitygen = ::Oclvanitygen
 $LOAD_PATH.unshift File.expand_path('../vanitygen_shim', __FILE__)
